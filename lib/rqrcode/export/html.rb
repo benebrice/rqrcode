9b2e70db013a2e55d56b3cf41cf74dee480a574b
Brice <brice.bene@gmail.com>
2020-08-23 14:55:40 +0200

    feat: Add style and classes to table
    Signed-off-by: Brice <brice.bene@gmail.com>

--- lib/rqrcode/export/html.rb
+++ lib/rqrcode/export/html.rb
@@ -5,8 +5,8 @@ module RQRCode
     module HTML
       #
       # Use this module to HTML-ify the QR code if you just want the default HTML
-      def as_html
-        ['<table>', rows.as_html, '</table>'].join
+      def as_html(options = {})
+        ['<table>', rows.as_html(options), '</table>'].join
       end
 
       private
@@ -16,8 +16,8 @@ module RQRCode
       end
 
       class Rows < Struct.new(:qr)
-        def as_html
-          rows.map(&:as_html).join
+        def as_html(options = {})
+          rows.map{|r| r.as_html(options)}.join
         end
 
         def rows
@@ -26,22 +26,31 @@ module RQRCode
       end
 
       class Row < Struct.new(:qr, :qr_module, :row_index)
-        def as_html
-          ['<tr>', cells.map(&:as_html).join, '</tr>'].join
+        def as_html(options = {})
+          style = options.dig(:style, :tr, :css) || ''
+          classes = options.dig(:style, :tr, :class) || ''
+          ["<tr style=\"#{style}\" class=\"#{html_class(classes)}\">", cells.map{|c| c.as_html(options)}.join, '</tr>'].join
         end
 
         def cells
           qr.modules.each_with_index.map { |qr_module, col_index| Cell.new(qr, col_index, row_index) }
         end
+
+        def html_class(classes)
+          classes
+        end
       end
 
       class Cell < Struct.new(:qr, :col_index, :row_index)
-        def as_html
-          "<td class=\"#{html_class}\"></td>"
+        def as_html(options = {})
+          style = options.dig(:style, :td, :css) || ''
+          classes = options.dig(:style, :td, :class) || ''
+          "<td style=\"#{style}\" class=\"#{html_class(classes)}\"></td>"
         end
 
-        def html_class
-          qr.checked?(row_index, col_index) ? 'black' : 'white'
+        def html_class(classes)
+          res = qr.checked?(row_index, col_index) ? 'black' : 'white'
+          res + ' ' + classes
         end
       end
     end
